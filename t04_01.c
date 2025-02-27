00ee7d1b192bb53ac43b2fde06e34f0677083f6a
OliviaTambunan1 <olivtmbn@gmail.com>
2025-02-27 14:24:09 +0700

    pair 22 selesai

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,31 @@
-// NIM - Name
-// NIM - Name
+// 12S24016 - Boy Harendy Simamora
+// 12S24048 - Olivia Tambunan
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+  char input[100];
+    char output[100];
+    int range, index = 0, j = 0;
+
+    scanf("%s", input);
+
+    range = strlen(input);
+
+
+    while (index < range) {
+        char temp[4] = {0};
+        strncpy(temp, input + index, 3);
+        int ascii = atoi(temp);
+        output[j++] = (char)ascii;
+        index += 3;
+    }
+    output[j] = '\0';
+
+    printf("Output: %s\n", output);
+
 
   return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,22 @@
-// NIM - Name
-// NIM - Name
+// 12S24016 - Boy Harendy Simamora
+// 12S24048 - Olivia Tambunan
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+    char input[100];
+    
+    fgets(input, sizeof(input), stdin);
 
-  return 0;
-}
+
+    input[strcspn(input, "\n")] = '\0';
+
+    for (int i = 0; i < strlen(input); i++) {
+        printf("%03d", (int)input[i]);
+    }
+    
+    printf("\n");
+
+    return 0;
+  } 
